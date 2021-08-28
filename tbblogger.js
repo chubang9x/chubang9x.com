9ae80206eedc49b5034d8fc25bb65a13e0114896
chubang9x <89292953+chubang9x@users.noreply.github.com>
2021-08-29 00:20:22 +0700

    Add files via upload

---
+++ tbblogger.js
@@ -0,0 +1,3 @@
+let EFV='<a href="https://www.truongblogger.top" style="position:absolute;z-index:-9999;left:-100%;">TruongBlogger</a>'
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
+// new Date().toString().includes('Vietnam')
