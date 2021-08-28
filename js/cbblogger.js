f8fd767b55cb4347d5ebaf69b2c76c41e51c83be
chubang9x <89292953+chubang9x@users.noreply.github.com>
2021-08-29 00:21:26 +0700

    Update and rename tbblogger.js to js/cbblogger.js

--- js/cbblogger.js
+++ js/cbblogger.js
@@ -1,3 +1,3 @@
-let EFV='<a href="https://www.truongblogger.top" style="position:absolute;z-index:-9999;left:-100%;">TruongBlogger</a>'
+let EFV='<a href="https://www.chubanblogger.blogspot.com" style="position:absolute;z-index:-9999;left:-100%;">ChuBangBlogger</a>'
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}else{document.body.insertAdjacentHTML('beforeend',EFV);}
 // new Date().toString().includes('Vietnam')
